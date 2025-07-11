48dc40ac7ae700e4bc122f6e490326b995ad332f
Pankzzzz <pankaj325649@gmail.com>
2025-07-11 14:36:46 +0530

    day 2

--- Day_2/datatype.py
+++ Day_2/datatype.py
@@ -10,3 +10,37 @@ n=27
 print(type(n))
 # int datatype
 
+m=27.9
+print(type(m))
+# float datatype
+
+t= ["apple"]
+print(type(t))
+# list datatype
+
+s= ("apple")
+print(type(s))
+# tuple datatype
+
+c= range(6)
+print(type(c))
+# range datatype
+
+k = {"name" : "PAnkaj", "age" :18}
+print(type(k))
+# dict datatype
+
+z= frozenset({"apple", "banana", "cherry"})
+print(type(z))
+# frozenset datatype
+
+d = bytearray(5)
+print(type(d))
+# bytearray datatype
+f = memoryview(bytes(5))
+print(type(f))	
+# memoryview datatype
+
+y = None
+print(type(y))
+# none datatype
---
+++ Day_2/testcode.py
@@ -0,0 +1,4 @@
+name= input("Enter your name")
+age=int(input("Enter your age"))
+is_adult=age>=18
+print(f"Your name is {name} and your age is {age} Are you an adult",is_adult)
