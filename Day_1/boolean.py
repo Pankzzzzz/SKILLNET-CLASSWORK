32665680d1b82de845da6ffd91783647edb8f1fe
Pankzzzz <pankaj325649@gmail.com>
2025-07-11 14:33:58 +0530

    day 2

--- Day_1/boolean.py
+++ Day_1/boolean.py
@@ -1,4 +1,3 @@
-
 age=int(input("Enter your age"))
 is_adult=age>=18
 print(f"Are you an adult",is_adult)
---
+++ Day_2/datatype.py
@@ -0,0 +1,12 @@
+p="Pankaj"
+print(type(p))
+# str datatype
+
+x=3j
+print(type(x))
+# complex datatype
+
+n=27
+print(type(n))
+# int datatype
+
